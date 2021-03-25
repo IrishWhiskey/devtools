002b9a5dd68e4813b9d072a367ce32760d3bbb8f
Irish Whiskey <47793460+IrishWhiskey@users.noreply.github.com>
2021-03-25 13:19:32 +0100

    add neovim

---
+++ nvim/remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "nvim remove"
+
+rm -rf /home/$(logname)/.config/coc
+rm -rf /home/$(logname)/.config/nvim
+
+#remove neovim
+if command -v nvim &>/dev/null; then
+	apt remove -y neovim
+    apt autoremove -y
+fi
