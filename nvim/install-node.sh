102a1b39ceb9391a20d46766b0bb710f06cbafd3
Vincenzo Territo <territo.vincenzo17@gmail.com>
2021-07-09 09:53:54 +0000

    nvim: small fix

--- nvim/install-node.sh
+++ nvim/install-node.sh
@@ -12,4 +12,5 @@ echo 'export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> $HOME/.zshrc
 
-nvm install node
+nvm install 16.4.0
+nvm use v16.4.0
