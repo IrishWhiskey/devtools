96d848538f46a5686528600ffa721971e83437a6
Vincenzo Territo <territo.vincenzo17@gmail.com>
2024-03-12 19:31:07 +0100

    nvim: install from appimage instead of tarball

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 install_nvim() {
-    wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-    tar -xf nvim-linux64.tar.gz
-    cp nvim-linux64/bin/nvim /usr/local/bin/nvim
+    wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+    chmod +x nvim.appimage
+    cp nvim.appimage /usr/local/bin/nvim
     ln -s /usr/local/bin/nvim /usr/local/bin/vim
 }
 
