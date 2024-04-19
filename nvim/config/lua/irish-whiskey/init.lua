6b4c015e66c89441cebd90c2f650521a5910e69d
Vincenzo Territo <territo.vincenzo17@gmail.com>
2024-04-19 17:58:14 +0200

    setup python lsp

--- nvim/config/lua/irish-whiskey/init.lua
+++ nvim/config/lua/irish-whiskey/init.lua
@@ -1,3 +1,4 @@
 require("irish-whiskey.set")
 require("irish-whiskey.remap")
 require("irish-whiskey.init_lazy")
+require("irish-whiskey.mason")
--- nvim/config/lua/irish-whiskey/mason.lua
+++ nvim/config/lua/irish-whiskey/mason.lua
@@ -1,32 +1,29 @@
-return {
-    init = function()
-        local pylsp = require("mason-registry").get_package("python-lsp-server")
-        pylsp:on("install:success", function()
-            local function mason_package_path(package)
-                local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
-                return path
-            end
-
-            local path = mason_package_path("python-lsp-server")
-            local command = path .. "/venv/bin/pip"
-            local args = {
-                "install",
-                "-U",
-                "pylsp-rope",
-                "python-lsp-black",
-                "python-lsp-isort",
-                "python-lsp-ruff",
-                "pyls-memestra",
-                "pylsp-mypy",
-            }
-
-            require("plenary.job")
-                :new({
-                    command = command,
-                    args = args,
-                    cwd = path,
-                })
-                :start()
-        end)
+local pylsp = require("mason-registry").get_package("python-lsp-server")
+pylsp:on("install:success", function()
+    local function mason_package_path(package)
+        local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
+        return path
     end
-}
+
+    local path = mason_package_path("python-lsp-server")
+    local command = path .. "/venv/bin/pip"
+    local args = {
+        "install",
+        "-U",
+        "python-lsp-isort",
+        "python-lsp-ruff",
+        "pylsp-mypy",
+    }
+    print("installing pylsp dependencies...")
+
+    require("plenary.job")
+        :new({
+            command = command,
+            args = args,
+            cwd = path,
+            on_exit = function(j, return_val)
+                print("installation done")
+            end,
+        })
+        :start()
+end)
