f54274477f0d50b0c494dd2629dc06d446c8b6f9
Vincenzo Territo <47793460+IrishWhiskey@users.noreply.github.com>
2024-06-23 09:49:54 +0200

    create nvim dir before copying config files

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -4,4 +4,7 @@ echo "nvim setup"
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-cp -rf ${DIR}/config/* ${HOME}/.config/nvim
+TARGET_DIR=${HOME}/.config/nvim
+
+mkdir -p ${TARGET_DIR}
+cp -rf ${DIR}/config/* ${TARGET_DIR}
