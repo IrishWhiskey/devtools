308647954d2cee5876447434167670545fe2bcb9
Vincenzo Territo <territo.vincenzo17@gmail.com>
2024-07-07 11:43:36 +0200

    nvim: remove commands for each OS

--- nvim/remove.sh
+++ nvim/remove.sh
@@ -2,8 +2,31 @@
 
 echo "nvim remove"
 
-rm -rf /home/$(logname)/.config/nvim
+OS=$(uname -s)
+if [[ "$OS" == "Linux" ]]; then
+    OS_version=$(uname -v)
 
-# remove neovim
-rm -f /usr/local/bin/vim
-rm -f /usr/local/bin/nvim
+    if [[ "$OS_version" == *"Ubuntu"* ]]; then
+        apt-get remove -y neovim vim
+    elif [[ "$OS_version" == *"Debian"* ]]; then
+        apt-get remove -y neovim vim
+    elif [[ "$OS_version" == *"CentOS"* ]]; then
+        yum remove -y neovim vim
+    elif [[ "$OS_version" == *"RHEL"* ]]; then
+        yum remove -y neovim vim
+    elif [[ "$OS_version" == *"Fedora"* ]]; then
+        dnf remove -y neovim vim
+    elif [[ "$OS_version" == *"Arch"* ]]; then
+        pacman -Rn neovim vim
+    else
+        echo "Unknown OS version"
+    fi
+
+    rm -f /usr/local/bin/vim
+    rm -f /usr/local/bin/nvim
+    rm -rf /home/$(logname)/.config/nvim
+elif [[ "$OS" == "Darwin" ]]; then
+    brew uninstall -y neovim vim
+else
+    echo "Unknown OS"
+fi
